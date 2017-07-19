5c0eaaab35d7f2a32306ce8a34944827b862103c
Cap'n Odin <albertjuhl@gmail.com>
2017-07-19 05:29:12 +0200

    Supports Python v3

---
+++ PySong.py
@@ -0,0 +1,208 @@
+import sys, os, getopt, re
+import preamble, auxiliary, style_tex
+from subprocess import call
+current_version = sys.version_info
+import pyPdf
+
+
+"""This function is used to create the tex file that is the songbook"""
+def create_sangbog(name, style, logo):
+	makeDirs()
+	
+	style = auxiliary.search_styles(style)	  #check if the specified style exist
+	
+	texPreamble = preamble.create_preamble(name, style, logo)	   #create the preamble of the tex file
+	
+	songLst = getSongs()
+
+	tex = addString("***SONGS***", songsToString(songLst, style), texPreamble)
+	
+	makeIndex(songLst)
+	
+	#call(["pdflatex", "-output-directory", "temp/", "temp/SongBooklet.tex"])
+	call(["pdflatex", "-output-directory", "temp/", "-jobname", "SongBook-" + name, "temp/SongBooklet.tex"])
+	
+	f = open("temp/SongBook-" + name + ".pdf", "rb")
+	pdf = pyPdf.PdfFileReader(f)
+	#print(pdf.trailer["/Root"]["/PageLabels"]["/Nums"])
+	
+	inconsistency = False
+	for song in songLst:
+		if("page" in song["options"]):
+			song["options"]["pagebool"] = isSongOnPage(song["title"], song["options"]["page"], pdf.pages)
+			if(not song["options"]["pagebool"]):
+				inconsistency = True
+				
+	f.close()
+	
+	if(inconsistency):
+		tex = addString("***SONGS***", songsToString(songLst, style), texPreamble)
+		call(["pdflatex", "-output-directory", "temp/", "-jobname", "SongBook-" + name, "temp/SongBooklet.tex"])
+	
+	addString("%***BOOKLET***", "", tex)
+	call(["pdflatex", "-output-directory", "temp/", "-jobname", "SongBook-" + name, "temp/SongBooklet.tex"])
+	
+	if(os.path.isfile("temp/" + "SongBook-" + name + ".pdf")):
+		os.replace("temp/" + "SongBook-" + name + ".pdf", "Booklet/" + "SongBook-" + name + ".pdf")
+
+def isSongOnPage(title, pageNum, pages):
+	boolean = False
+	index = 0
+	for page in pages:
+		if(index < len(pages) - 1):
+			if(re.search("\d+" + title.replace(" ", ""), page.extractText())): # if page contains the song
+				if(re.search(str(pageNum) + "$", page.extractText()) != None): # if page ends with the expected page number
+					boolean = True
+		index += 1
+	return boolean
+	
+
+def getSongs():
+	specialSongs = []
+	songs = []
+	for file in os.listdir("Songs/"):
+		if(file.endswith(".txt")):
+			song = fileRead("Songs/" + file)
+			line0 = song[:song.find("\n")]
+			options = line0[:line0.find("\\")]
+			title = re.search('(?<=song{)(.*?)}', line0)
+			if(title != None):
+				title = title.group(1)
+				if(options != ""):
+					specialSongs.append({"title" : title, "text" : song[song.find("\\"):], "options" : eval(options)})
+				else:
+					songs.append({"title" : title, "text" : song[song.find("\\"):], "options" : {}})
+	
+	sortedSpecialSongs = sorted(specialSongs, key = lambda song: song["options"]["num"])
+	for song in sortedSpecialSongs:
+		songs.insert(song["options"]["num"], song)
+		
+	return songs
+
+
+def songsToString(songs, style):
+	songsStr = ""
+	next_page = 0
+	counter = 0
+	bool = False
+	linesPerPage = 64 * 2
+	
+	for song in songs:
+		text = ""
+		j = song["text"].find("]")
+		
+		before = song["options"]["pagebool"] if "pagebool" in song["options"] else True
+		
+		if("page" in song["options"] and before):
+			tempStyle = song["options"]["style"] if "style" in song["options"].keys() else "arabic"
+			text += "\\setcounter{temppage}{\\value{page}}\n\\pagenumbering{" + tempStyle + "}\n\\setcounter{page}{" + str(song["options"]["page"]) + "}\n"
+			next_page = linesPerPage
+			bool = True
+			
+		if("num" in song["options"].keys()):
+			text += "\\setcounter{temp}{\\thesongnum}\n\\setcounter{songnum}{" + str(song["options"]["num"]) + "}"
+			text += song["text"][:j+1] + "\\hypertarget{" + song["title"] + "}{}\n\\label{song" + str(counter) + "}\n" + song["text"][j+1:] + "\n"
+			text += "\n\\setcounter{songnum}{\\thetemp + 1}"
+		else:
+			next_page -= getNumOfLines(song["text"][j+1:])
+			if(bool and next_page < 0):
+				text += "\\pagenumbering{" + style + "}\n\\setcounter{page}{\\value{temppage} + 1}\n"
+				bool = False
+			text += song["text"][:j+1] + "\\hypertarget{" + song["title"] + "}{}\n\\label{song" + str(counter) + "}\n" + song["text"][j+1:] + "\n"
+		
+		if("page" in song["options"] and not before):
+			tempStyle = song["options"]["style"] if "style" in song["options"].keys() else "arabic"
+			text += "\\setcounter{temppage}{\\value{page}}\n\\pagenumbering{" + tempStyle + "}\n\\setcounter{page}{" + str(song["options"]["page"]) + "}\n"
+			next_page = linesPerPage * 1.5
+			bool = True
+			
+		songsStr += "\n" + text + "\n"
+		counter += 1
+	return songsStr
+
+	
+
+def getNumOfLines(song):
+	num = 0
+	song = re.sub("\\\w+", "a", song)
+	for line in re.split("\n+", song):
+		num += 1 + len(line) / 40
+	return num
+
+
+
+def makeIndex(songLst):
+	index_file = open("temp/titlefile.sbx",'w+', encoding = 'utf-8')
+	
+	for i in range(0, len(songLst)):
+		index_file.write("\\idxentry{" + songLst[i]["title"] + "}{Sang nummer: \\hyperlink{" + songLst[i]["title"] + "}{" + str(i) + "} På side: \pageref{song" + str(i) + "}}\n")
+	
+	index_file.close()
+
+
+def fileRead(file):
+	try:
+		text = open(file, 'r', encoding = 'utf-8-sig').read()
+	except:
+		text = open(file, 'r').read()
+	return text
+
+
+def addString(tag, string, tex):
+	f = open("temp/SongBooklet.tex", 'w+', encoding = 'utf-8')
+	tex = tex.replace(tag, string)
+	f.write(tex)
+	f.close()
+	return tex
+	
+
+def makeDirs():
+	if not os.path.isdir("temp"):
+		os.mkdir("temp")
+	if not os.path.isdir("Booklet"):
+		os.mkdir("Booklet")
+	if not os.path.isdir("Songs"):
+		os.mkdir("Songs")
+	
+	
+def usage():
+	print("Usage: " + sys.argv[0] + " -p <used to define new pagenumbering style> -s <choose pagenumbering style> -n <name of sangbook> -l <file for logo, svg or png>")
+	print("Options: -c (if it is a camp) -u (if it is UNF) -e (if you do no want a front page) -S (if you want the songs to be sorted by title) -f (if you want the songs to be sorted by a fixed number)")
+
+
+def main(argv):
+	name = ""		   #name of the songbook
+	style = ""		  #the chosen style
+	logo = ""		   #the file containing the logo for the front page
+	try:
+		opts, args = getopt.getopt(argv, "hs:n:l:p:", ["help", "style=", "name=", "logo="])
+	except getopt.GetoptError:
+		usage()
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt in ("-h","--help"):		  #option used to print usage
+			usage()
+			sys.exit()
+		elif opt in ("-s", "--number_style"):	   #option used to set the pagenumber style in the tex file
+			style = arg
+		elif opt in ("-n", "--name"):		   #option used to set the name
+			name = arg
+		elif opt in ("-l", "--logo"):		   #option used to get a logo on the front page
+			logo = arg
+		elif opt in ("-p", "--style"):		  #option used for making a new pagenumber style
+			new_style = arg
+			n = new_style.split(" ")[0]		#split the argument into two parts, a name
+			s = new_style.split(" ")[1]		#and the style, that will be a regular expression
+			if auxiliary.search_styles(n) != n:		 #check if the name is already in use, cannot have two styles by the same name
+				style_tex.new_page_style(n, s)
+				style = n
+			else:
+				print("There is already a style with that name.")
+		else:
+			usage()
+			sys.exit()
+			
+	create_sangbog(name, style, logo)		 #call to create sangbog
+
+if __name__=='__main__':
+	sys.exit(main(sys.argv[1:]))
--- preamble.py
+++ preamble.py
@@ -1,126 +1,40 @@
 # -*- coding: utf-8 -*-
-import sys, os, getopt, re
+import re, os, shutil
 from PIL import Image
-from reportlab import pdfbase
-#    from svglib.svglib import svg2rlg
-#else:
-from svglibbuild3 import svg2rlg
+from svglib.svglib import svg2rlg
+
 from reportlab.graphics import renderPDF
 
-standard = ["arabic", "roman", "Roman", "alph", "Alph","binary","hex","oct"]
+def create_preamble(name, style, logo):
+	if ".svg" in logo:		  #check if the logo is in svg format
+		tempf = open(logo, 'r')	  #open the file for reading
+		s = tempf.read()
+		height_k = re.search('height=\"(\d*).(\d*)', s)		  #get the height of the picture, using regular expressions
+		height_l = float(re.search('\"(\d*).(\d*)', height_k.group(0)).group(0)[1:])		 #get only the height
 
+		width_k = re.search('width=\"(\d*).(\d*)', s)			#get the width of the picture, using regular expressions
+		width_l = float(re.search('\"(\d*).(\d*)', width_k.group(0)).group(0)[1:])		   #get only the width
+		
+		drawing = svg2rlg(logo)		 #draw the picture
+		logo = logo.replace(".svg", ".pdf")		 #replace the .svg part with .pdf
+		renderPDF.drawToFile(drawing, "temp/" + logo[logo.rfind("/") + 1:])			#draw the picture to the file and render it
+		
+	else:
+		width_l, height_l = Image.open(logo).size
+		if(os.path.isfile(logo)):
+			shutil.copy(logo, "temp/" + logo[logo.rfind("/") + 1:])
+			
+	logo = logo[logo.rfind("/") + 1:]
+	
+	scale_height = 622.519685 / height_l
+	scale_width = 418.110236 / width_l
+	scale = (min(scale_height, scale_width))		 #calculate how much the logo can be scaled
+	
+		
+	f = open("Resources/SongBookletTemplate.tex", 'r', encoding = 'utf-8')		#now create the tex file for the songbook itself
 
+	tex = f.read()
+	f.close()
+	
+	return tex.replace("***NAME***", name).replace("***LOGO***", logo).replace("***NUMBERING***", style).replace("***SCALE***", str(scale * 0.4))
 
-
-def create_preamble(author, name, style, logo, empty):
-    if """.svg""" in logo:          #check if the logo is in svg format
-        tempf = open(logo,'r')      #open the file for reading
-        s = tempf.read()
-        height_k = re.search('height=\"(\d*).(\d*)',s)          #get the height of the picture, using regular expressions
-        height_l = re.search('\"(\d*).(\d*)',height_k.group(0)).group(0)[1:]         #get only the height
-        height_l = float(re.sub("[^0-9]", "", height_l))
-        
-        width_k = re.search('width=\"(\d*).(\d*)',s)            #get the width of the picture, using regular expressions
-        width_l = re.search('\"(\d*).(\d*)',width_k.group(0)).group(0)[1:]           #get only the width
-        width_l = float(re.sub("[^0-9]", "", width_l))
-
-        drawing = svg2rlg(logo)         #draw the picture
-        file_name = str.split(logo, """.svg""")[0] + """.pdf"""         #replace the .svg part with .pdf
-        renderPDF.drawToFile(drawing, file_name)            #draw the picture to the file and render it
-    if not empty:           #if it is anything but empty and a svg file
-        tempf = open(name+"Camp_tekst.tex",'w+')            #we make a new tex file to be used for front page
-        tempf.write("""\\documentclass[pdftex]{article}""")         #now we start to declare the preamble for that tex file
-        tempf.write("""\\title{Sangbog for """ + name + """ \\theyear}""")
-        if author != "":
-            tempf.write("""\\author{"""+author+"""}""")           #we write the title for the songbook to the tex file
-        tempf.write("""\\date{\\today}
-\\begin{document}
-\\pagestyle{empty}
-\\begin{center}""")     #start the document
-        tempf.write("""\\fontfamily{phv}\\selectfont\\Huge """+name+""" \\the\\year\n""")         #otherwise just write name
-        tempf.write("""\\end{center}
-
-\\end{document}""")         #end the document
-
-        from subprocess import call
-        tempf.close()
-        call(["pdflatex", name+"Camp_tekst.tex"])       #use pdflatex on the tex file
-
-
-    f = open("Sanghaefte.tex", 'w+')        #now create the tex file for the songbook itself
-
-    f.write("""\\documentclass[11pt, pdftex]{article}
-\\usepackage{latexsym,fancyhdr}
-\\usepackage[a4paper,includeheadfoot,margin=2.5cm]{geometry}
-\\usepackage[lyric]{songs}
-\\usepackage[utf8]{inputenc}
-%\\usepackage[danish, english]{babel}
-\\usepackage[english]{babel}
-\\usepackage{amssymb}
-\\usepackage{stmaryrd}
-\\usepackage{amsmath}
-\\usepackage{amsthm}
-\\usepackage[final]{pdfpages}
-\\usepackage[none]{hyphenat}
-\\usepackage{hyperref}
-\\usepackage{multicol}
-\\input binhex
-\\setlength{\columnsep}{2cm}
-\\newindex{titleidx}{titlefile}
-\\sepindexesfalse
-\\title{Sangbog """ + name + """ \\the\\year}\n""")           #create preamble
-    if author != "":
-        f.write("""\\author{"""+author+"""}\n""")        #
-    f.write("""\\date{\\today}
-\\addtolength{\\headwidth}{\\marginparsep}
-\\addtolength{\\headwidth}{\\marginparwidth}
-\\renewcommand{\\headrulewidth}{0.4pt}
-\\renewcommand{\\footrulewidth}{0.4pt}
-\\fancyhead[LE,RO]{\\LHeadFont Sangbog}
-\\fancyhead[CE,CO]{\\CHeadFont\\thepage}
-\\fancyhead[RE,LO]{\\RHeadFont\\RelDate}\n""")
-    if """renew""" in style:
-        f.write("""""" + style + """\n""")
-    else:
-        f.write("""\\input{page_numbering}
-\\pagenumbering{""" + style + """}\n""")
-    f.write("""\\begin{document}
-\\newcounter{temp}
-\\newcounter{temppage}\n""")          #continue preamble
-    if not empty:       #if empty is not specified start writing a front page
-        f.write("""\\thispagestyle{empty}
-\\centering
-\\phantom{test}
-\\vspace{1cm}\n""")          
-        if """.svg""" in logo:
-            scale_height = 1122.519685 / height_l
-            scale_width = 818.110236 / width_l
-            scale = (min(scale_height, scale_width))         #calculate how much the logo can be scaled
-            new_height = scale*height_l
-            new_width = scale*width_l
-            f.write("""\\mbox{\\includegraphics[scale="""+str(scale)+"""]{"""+file_name+"""}}\n""")       #include the logo in the tex file and scale it
-        elif """.jpg""" in logo or """.png""" in logo:      #if its not a vector graphic image
-            img = Image.open(logo)
-            width, height = img.size            #get the height and width of the image
-            scale_height = 1122.519685 / height
-            scale_width = 818.110236 / width
-            scale = min(scale_width, scale_height)      #get the smallest of the two 
-            if scale < 1:       #if the lowest is less than 1 we need to scale down so it can fit in the page, otherwise we do no scale up
-                f.write("""\\mbox{\\includegraphics[scale="""+str(scale)+"""]{"""+logo+"""}}\n""")
-            else:
-                f.write("""\\mbox{\\includegraphics[scale="""+str(scale)+"""]{"""+logo+"""}}\n""")
-        f.write("""\\vspace{1cm}
-\\begin{center}\n""")
-        f.write("""\\fontfamily{phv}\\selectfont\\Huge """+name+""" \\the\\year\n""")         #put the title for the songbook below the logo
-        f.write("""\\end{center}\n""")
-    f.write("""\\vspace{2.5cm}
-\\newpage
-\\setcounter{page}{0}
-\\raggedright
-\\songpos{0}
-\\spenalty=-10
-\\vvpenalty=100
-\\begin{songs}{}
-\\setcounter{songnum}{0}
-""")        #end the preamble
-    f.close()       #and close the file
