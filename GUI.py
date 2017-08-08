8953d047cc39aff31e368c3c6d8f33c989d9a175
Cap'n Odin <albertjuhl@gmail.com>
2017-08-08 04:24:03 +0200

    Added access to lib folder

--- GUI.py
+++ GUI.py
@@ -1,6 +1,11 @@
 
-import time
-	
+import os, sys, inspect, time
+
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0])) + "\\lib"
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
+
 import tkinter as tk
 from tkinter import ttk, font, filedialog
 
