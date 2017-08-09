49d78a98e8f0eed508d13beac7cbde386069f763
Cap'n Odin <albertjuhl@gmail.com>
2017-08-09 03:36:58 +0200

    Separated functionality into functions

--- auxiliary.py
+++ auxiliary.py
@@ -10,12 +10,7 @@ def search_styles(style):
 	if style == "":		 #if not style is defined just return arabic
 		style = "arabic"
 	else:
-		f_style = open("Resources/page_numbering.tex", 'r')		   #open the tex file for reading only
-		text = f_style.read()
-		list_styles = re.findall('\@\w*}',text)	 #use a regular expressions to get the strings where a specific combination of chars occur
-		for i in range(len(list_styles)):		   #go through the list of strings found
-			list_styles[i] = list_styles[i][1:len(list_styles[i])-1]		#remove the first char from all entries in the list
-		list_styles = list_styles + standard		  #concat the list with the standard pagenumber style
+		list_styles = getStyles()
 		if style not in list_styles:		#check if the style is in the list
 			style = "arabic"		#if not return arabic
 	if style == "hex":
@@ -25,3 +20,13 @@ def search_styles(style):
 	elif style == "oct":
 		style = "octX"
 	return style		#otherwise return the style
+
+def getStyles():
+	f_style = open("Resources/page_numbering.tex", 'r')		   #open the tex file for reading only
+	text = f_style.read()
+	list_styles = re.findall('\@\w*}',text)	 #use a regular expressions to get the strings where a specific combination of chars occur
+	for i in range(len(list_styles)):		   #go through the list of strings found
+		list_styles[i] = list_styles[i][1:len(list_styles[i])-1]		#remove the first char from all entries in the list
+		
+	return list_styles + standard		  #concat the list with the standard pagenumber style
+	
